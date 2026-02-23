78c0f75c47a361c6dbc45c1cfd479c98a4590a4b
soanseng <soanseng@anatomind.com>
2026-02-23 22:55:49 +0800

    feat: add HomeScreen composable and MainActivity (TDD)

---
+++ app/src/main/java/com/voxink/app/ui/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.voxink.app.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.voxink.app.ui.theme.VoxInkTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VoxInkTheme {
+                HomeScreenContent()
+            }
+        }
+    }
+}
