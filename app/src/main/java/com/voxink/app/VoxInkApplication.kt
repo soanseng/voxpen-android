1adeed0b11211c64eb5ea104811366b5f5980c91
soanseng <soanseng@anatomind.com>
2026-02-23 22:54:13 +0800

    feat: add VoxInkApplication with Hilt and Timber (TDD)

---
+++ app/src/main/java/com/voxink/app/VoxInkApplication.kt
@@ -0,0 +1,16 @@
+package com.voxink.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class VoxInkApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
