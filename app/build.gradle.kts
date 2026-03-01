2e8248a11587ea31709c512671db0315668ef1ac
soanseng <soanseng@anatomind.com>
2026-03-01 20:41:39 +0800

    chore: bump version to 1.2.0 (versionCode 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.voxpen.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
